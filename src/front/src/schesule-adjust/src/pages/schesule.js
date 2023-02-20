e23315865c93a87da4385f8ce4bcced8d29c4233
shige1114 <g2054029@gmail.com>
2023-02-21 02:12:49 +0900

    test bot

--- src/front/src/schesule-adjust/src/pages/schesule.js
+++ src/front/src/schesule-adjust/src/pages/schesule.js
@@ -2,9 +2,8 @@ import { useLayoutEffect, useEffect, useState } from 'react';
 
 import Day from "@/components/Day"
 import { getDays } from "pages/api/getDays"
-import Time from '@/components/Time';
 import { Button } from '@mui/material';
-import styles from '../styles/Home.module.css'
+
 export default function Schesule() {
     const [days, setDays] = useState([])
     useEffect(() => {
@@ -18,13 +17,7 @@ export default function Schesule() {
     return (
         <>
             <h2>3月</h2>
-            <table >
-                <td><Button variant="text" color="primary">6</Button></td>
-                <td><Button variant="text" color="primary">7</Button></td>
-                <td><Button variant="text" color="primary">8</Button></td>
-                <td><Button variant="text" color="primary">9</Button></td>
-                <td><Button variant="text" color="primary">10</Button></td>
-            </table>
+            
             <Day props={days} />
         </>
 
